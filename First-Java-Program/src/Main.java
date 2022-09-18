d482c9e0ddf057896efc3f79375a7615db895254
Prashant Bhattacharya <prashantbhattacharya72@gmail.com>
2022-09-19 01:50:06 +0530

    First-Java_Program Assignment.

---
+++ First-Java-Program/src/Main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//  1. - Write a program to print whether a number is even or odd, also take input from the user.
+//        int num = 0;
+//        System.out.print("Enter the number which you want to check: ");
+//        Scanner s = new Scanner(System.in);
+//        num = s.nextInt();
+//        if(num % 2 == 0){
+//            System.out.println("The Number which you have entered is: EVEN");
+//        } else {
+//            System.out.println("The Number which you have entered is: ODD");
+//        }
+
+//  2. - Take name as input and print a greeting message for that particular name.
+//        String name = "";
+//        System.out.print("Please enter your Full Name: ");
+//        Scanner s = new Scanner(System.in);
+//        name = s.nextLine();
+//        System.out.println("Hello " + name + ". Welcome Back! Have a Nice Day.");
+
+//  3. - Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+//        int principal, rate, time, simpleInterest;
+//        Scanner s = new Scanner(System.in);
+//        System.out.print("Enter the Principal Value: ");
+//        principal = s.nextInt();
+//        System.out.print("Enter the Rate: ");
+//        rate = s.nextInt();
+//        System.out.print("Enter the Time: ");
+//        time = s.nextInt();
+//        simpleInterest = (principal * rate * time)/100 ;
+//        System.out.print("The Simple Interest for the above Principal amount, for the given Rate and for the given Time would be: " + simpleInterest);
+
+//  4. - Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+//        char operation;
+//        int n1, n2, result;
+//        Scanner s = new Scanner(System.in);
+//        System.out.print("Enter the First Number: ");
+//        n1 = s.nextInt();
+//        System.out.print("Enter the Second Number: ");
+//        n2 = s.nextInt();
+//        System.out.println("list of operations: +, -, *, /");
+//        System.out.print("Enter the operation you want to perform : ");
+//        operation = s.next().charAt(0);
+//        if(operation == '+'){
+//            result = n1 + n2;
+//            System.out.println("The sum of the given two numbers is: " + result);
+//        } else if (operation == '-') {
+//            result = n1 - n2;
+//            System.out.println("The Difference of the given two numbers is: " + result);
+//        } else if (operation == '*') {
+//            result = n1 * n2;
+//            System.out.println("The Product of the given two numbers is: " + result);
+//        } else if(operation == '/') {
+//            result = n1 / n2;
+//            System.out.println("The Division of the given two numbers is: " + result);
+//        } else {
+//            System.out.println("Invalid Operation");
+//        }
+
+//  5. - Take 2 numbers as input and print the largest number.
+
+    }
+}
